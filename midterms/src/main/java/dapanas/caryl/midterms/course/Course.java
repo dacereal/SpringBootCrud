b1f2aa59ea18f636057e1e44be27f228f32060a3
dacereal <dapanascaryl@gmail.com>
2025-03-26 14:22:36 +0800

    4th commit - fixed setters and getters

--- midterms/src/main/java/dapanas/caryl/midterms/course/Course.java
+++ midterms/src/main/java/dapanas/caryl/midterms/course/Course.java
@@ -29,11 +29,11 @@ public class Course {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
+    public String getSection() { return section; }
+    public void setSection(String author) { this.section = section; }
     
-    public String getCategory() { return category; }
-    public void setCategory(String category) {this.category = category; }
+    public String getCourses() { return courses; }
+    public void setCourses(String category) {this.courses = courses; }
 
     @Override
     public String toString() {
